679def9d0b9ac44639aef4e63f7b12955fb71d37
miguel alonso <migalonso@deloitte.com>
2023-09-29 23:09:10 -0600

    Java13 features

---
+++ src/BloquesTexto.java
@@ -0,0 +1,23 @@
+public class BloquesTexto {
+
+	public static void main(String[] args) {
+		// ANTES
+		String html = "<html>\n" +
+			    "   <body>\n" +
+			    "      <p class=\"text\">Hello, Escapes</p>\n" +
+			    "   </body>\n" +
+			    "</html>\n";
+		
+		System.out.println(html);
+		System.out.println("----------------------------------");
+		
+		// AHORA
+		String html2 = """
+			    <html>
+			        <body>
+			            <p class="text">Hello, Text Blocks</p>
+			        </body>
+			    </html>""";
+		System.out.println(html2);
+	}
+}
